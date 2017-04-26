5a9fec647c9024bb09d6e6ef533cfd5673343f6a
Braxton Plaxco <bplaxco@lupnix.org>
2017-04-26 19:49:40 -0400

    One last small text change. I think ^_^
    ~ B'ezrat Hashem ~

--- connectors/AbstractConnector.py
+++ connectors/AbstractConnector.py
@@ -1,7 +1,6 @@
 class AbstractConnector(object):
     """
-    This is the abstract class for a connector. Every connector should implement
-    these methods
+    Every connector should implement these methods.
     """
 
     def __init__(self, client_id, channel, server, port, debug=False):
