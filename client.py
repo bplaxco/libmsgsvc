9d42f2385e49a09d2f0bc4b682b74c674b2d2ff4
Jonathan Pittard <jpittard@redhat.com>
2017-04-27 22:11:56 -0400

    Simplifying the client

--- client.py
+++ client.py
@@ -3,7 +3,7 @@
 from AbstractClient import AbstractClient
 
 def receive(bus, msg):
-    print("Got "+msg.get_data())
+    print(msg.get_data())
 
 def main(bus):
     bus.send_data(raw_input(">").strip())
